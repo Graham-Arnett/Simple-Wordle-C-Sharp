d0134b62d2a0ab1b095329e6e97a6c34dfb169ff
Graham Arnett <GrahamKentArnett@outlook.com>
2025-02-25 14:57:31 -0500

    getting closer, it no longer just repeats that you've won if you try to replay and I can confirm the word changes between attempts, I think I made an error in defining how the amount of turns is displayed though

--- WordleApp/Program.cs
+++ WordleApp/Program.cs
@@ -14,16 +14,17 @@ namespace WordleApp
                 "\nYellow is correct but in the wrong place" +
                 "\nWhite is not used at all");
             //int turns = 0;//initializing at zero turns
-            bool victory = false; //if they have won
+            //bool victory = false; //if they have won
             //bool validInput = false;//if input is valid or not
             Random random = new Random();//because the word is chosen randomly
            
             string choice;//initialize choice, this is for the do/while loop
             string[] answers = {"FLOAT","BLAME","SHARP","IDIOT","MORON","GLOAT","CLOAK","BROKE","FIXED","PUSHY"};//possible answers for the game to choose
-            int turns = 0;
+            
             do
             {
-                //int turns = 6;
+                int turns = 0;
+                bool victory = false;
                 int pickedAnswer = random.Next(0,10);//we have to do the random inside the loop so it can change with each runthrough
                 string answer = answers[pickedAnswer];
                 while (!victory || turns == 5)
@@ -31,38 +32,43 @@ namespace WordleApp
                     Console.Write($"\nYour guess: ");
                     string entry = Console.ReadLine().ToUpper();
                     int strLength = entry.Length;
-                    if(entry.Length < 5 || entry.Length > 5)
+                    if (entry.Length < 5 || entry.Length > 5)
                     {
                         Console.WriteLine("All answers are precisely 5 letters long, this will not increment turns or provide a hint. Guess again.");
                     }
-                    for(int counter = 0; counter < answer.Length; counter++)
+                    else
                     {
-                       
-                        if (entry[counter] == answer[counter])
+                        //turns++;
+                        for (int counter = 0; counter < answer.Length; counter++)
                         {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                        }
-                        else if (answer.Contains(entry[counter]) && entry[counter] != answer[counter]) //if the letter is correct and the placement is not
-                        {
-                            Console.ForegroundColor= ConsoleColor.Yellow;
-                        }
-                       
 
-                        Console.Write($"" + entry[counter].ToString() + " "); //prints your guess
-                        Console.ForegroundColor= ConsoleColor.White;//reset color
-                      
-                     
-                    }
-                    if (entry == answer)
-                    {
-                        Console.Write("\nYou did it!");
-                        victory = true;
-                    }
-                    turns++; //increments the amount of turns you've had
-                    if(turns == 5)
-                    {
-                        Console.WriteLine("\nYou have lost.");
-                        victory = true;//Ik its odd, but thats the name of the variable for leaving
+                            if (entry[counter] == answer[counter])
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                            }
+                            else if (answer.Contains(entry[counter]) && entry[counter] != answer[counter]) //if the letter is correct and the placement is not
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                            }
+
+
+                            Console.Write($"" + entry[counter].ToString() + " "); //prints your guess
+                            Console.ForegroundColor = ConsoleColor.White;//reset color
+
+
+                        }
+                        if (entry == answer)
+                        {
+                            Console.Write("\nYou did it!");
+                            victory = true;
+                        }
+                        //turns++; //increments the amount of turns you've had
+                        if (turns == 5)
+                        {
+                            Console.WriteLine("\nYou have lost.");
+                            victory = true;//Ik its odd, but thats the name of the variable for leaving
+                        }
+                        turns++;
                     }
                     
                 }
